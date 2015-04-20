1eeb5aa6afd98d03c309b392c7cf6677a9ee9efa
bcostab13 <b.costab13@gmail.com>
2015-04-19 20:28:29 -0500

    Version 0.1 Prueba

---
+++ laba/src/main/java/example/laba/laba/cprueba.java
@@ -0,0 +1,37 @@
+package example.laba.laba;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+
+/**
+ * Created by Brenda on 19/04/2015.
+ */
+public class cprueba extends Activity{
+    Button izquierdo,derecho;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.prueba);
+
+        //asociamos
+        izquierdo=(Button)findViewById(R.id.buttonprueba);
+        derecho=(Button)findViewById(R.id.buttonprueba2);
+
+        Animation mov_izquierda;
+        mov_izquierda= AnimationUtils.loadAnimation(this,R.animator.ladoizquierdo);
+        mov_izquierda.reset();
+        izquierdo.startAnimation(mov_izquierda);
+
+        Animation mov_derecha;
+        mov_derecha= AnimationUtils.loadAnimation(this,R.animator.ladoderecho);
+        mov_derecha.reset();
+        derecho.startAnimation(mov_derecha);
+
+
+    }
+}
